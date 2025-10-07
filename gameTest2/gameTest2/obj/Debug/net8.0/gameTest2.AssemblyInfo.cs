c349e367b4ed38f3f46a824d2a2157cd80b336f8
pw10thu <pw10thu@eduvaud.ch>
2025-10-07 13:36:29 +0200

    Feat: Ajout des lasers du boss et resolu quelques bugs du fullscreen

--- gameTest2/gameTest2/obj/Debug/net8.0/gameTest2.AssemblyInfo.cs
+++ gameTest2/gameTest2/obj/Debug/net8.0/gameTest2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gameTest2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e314410fa4d7ef84fe0404e49215ffb11221b0e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e7db7fd92e82559d45c44085bf198b347af0628")]
 [assembly: System.Reflection.AssemblyProductAttribute("gameTest2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gameTest2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
